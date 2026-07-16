200e5c23efef7b95cdcc3ef4654c53d1fa693c1f
Aayush Mittal <mittalaayush570@gmail.com>
2026-07-16 22:05:16 +0530

    route modified2

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,4 +1,5 @@
-// src/config.ts
-
 export const API_URL =
   import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
+
+console.log("VITE_API_URL =", import.meta.env.VITE_API_URL);
+console.log("API_URL =", API_URL);
