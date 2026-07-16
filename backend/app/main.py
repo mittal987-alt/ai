8758335a9a6c42fecabf30a7694e0007bdc0e3ac
Aayush Mittal <mittalaayush570@gmail.com>
2026-07-16 19:03:08 +0530

    route modify

--- backend/app/main.py
+++ backend/app/main.py
@@ -102,9 +102,14 @@ except Exception as e:
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:5173",
+    "https://ai-w27z.vercel.app",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
